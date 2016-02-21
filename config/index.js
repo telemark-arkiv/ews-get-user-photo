5782a9012d2f28e7f7c0d4319f41ff70f3aad621
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-02-21 13:12:11 +0100

    Updates appVersion

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ process.env.EWS_DOMAIN = 'login'
 
 var config = {
   appName: 'ews-get-user-photo',
-  appVersion: '1.0.0',
+  appVersion: '2.0.0',
   url: process.env.EWS_URL || 'https://epost.vfk.no/ews/Exchange.asmx/s/GetUserPhoto?email=@username@vfk.no&size=HR@sizex@size',
   user: process.env.EWS_USER || 'user',
   pass: process.env.EWS_PASS || 'pass',
